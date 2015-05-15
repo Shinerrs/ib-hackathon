c6c2bc7f73b79b035c7d5a49758f329ff53c3139
Miroslav Milivojevic <Miroslav.Milivojevic@infobip.com>
2015-05-15 17:14:24 +0200

    Monitor request

---
+++ classes/Consts.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmilivojevic
+ * Date: 5/15/15
+ * Time: 5:03 PM
+ */
+
+define("SERVICE_URL", "http://localhost:3300/");
--- classes/Monitor.php
+++ classes/Monitor.php
@@ -6,12 +6,25 @@
  * Time: 4:58 PM
  */
 
+require_once "Consts.php";
+
 class Monitor {
 
     private $stream;
     private $positionX;
     private $positionY;
     private $ip;
+
+    function __construct()
+    {
+        $response = http_get(SERVICE_URL, null, $info);
+        $data = json_decode($response);
+
+        $this->stream = $data["stream"];
+        $this->positionX = $data["positionX"];
+        $this->positionY = $data["positionY"];
+        $this->ip = $data["ip"];
+    }
 
 
 }
