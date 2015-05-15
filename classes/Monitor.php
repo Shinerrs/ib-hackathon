69cfb47193bf0b6e356ce54d5474343b2f7d97ce
Miroslav Milivojevic <Miroslav.Milivojevic@infobip.com>
2015-05-15 17:02:23 +0200

    Monior class

---
+++ classes/Monitor.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmilivojevic
+ * Date: 5/15/15
+ * Time: 4:58 PM
+ */
+
+class Monitor {
+
+    private $stream;
+    private $positionX;
+    private $positionY;
+    private $ip;
+
+
+}
